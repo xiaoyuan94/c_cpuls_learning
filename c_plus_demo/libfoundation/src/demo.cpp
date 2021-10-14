ced5ff201ba3a75936057c49d80189cb862075dd
xuxiaoyuan <xuxiaoyuan072@chinasoftinc.com>
2021-10-14 17:30:22 +0800

    Template demo

--- c_plus_demo/libfoundation/src/demo.cpp
+++ c_plus_demo/libfoundation/src/demo.cpp
@@ -11,5 +11,5 @@ void Demo::show()
     std::cout << " Demo show start " << std::endl;
     TemplateDemo<std::string, std::string> templateDemo("hello ", "world");
     templateDemo.show();
-    std::cout << " Demo show end" << std::endl;
+    std::cout << " Demo show end \n" << std::endl;
 }
--- c_plus_demo/start/src/main.cpp
+++ c_plus_demo/start/src/main.cpp
@@ -4,9 +4,9 @@
 
 int main(int argc, char *argv[])
 {
-    std::cout << "main start " << std::endl;
+    std::cout << "main start \n" << std::endl;
     Demo demo;
     demo.show();
-    std::cout << "main end " << std::endl;
+    std::cout << "main end \n" << std::endl;
     return 0;
 }
